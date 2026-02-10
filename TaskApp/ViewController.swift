6d0518353525c86a3bc431d71b19bbb57d4b5151
LalaAghyva <laleagayeva78@gmail.com>
2026-02-10 10:39:36 +0400

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Lala on 10.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
